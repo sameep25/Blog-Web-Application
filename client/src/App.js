cba274e517eda18a312c0a6323233170c662a44b
sameep25 <sameepvishwakarma@gmail.com>
2022-07-01 07:41:03 -0700

    intial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import Login from './components/account/Login';
+
+function App() {
+  return (
+    <div className="App">
+      Index
+    </div>
+  );
+}
+
+export default App;
